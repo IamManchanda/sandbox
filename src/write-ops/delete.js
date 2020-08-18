83a19504b6bcbd39ef80c3892bb48c74ffc33ada
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-08-18 16:02:42 +0530

    ops:write:delete

---
+++ src/write-ops/delete.js
@@ -0,0 +1,21 @@
+const { config, DynamoDB } = require("aws-sdk");
+
+config.update({ region: "eu-west-1" });
+const documentClient = new DynamoDB.DocumentClient();
+
+documentClient.delete(
+  {
+    TableName: "td_notes_sdk",
+    Key: {
+      user_id: "bb",
+      timestamp: 1597745641,
+    },
+  },
+  (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.stringify(data, null, 2));
+    }
+  },
+);
