46e48674c095230d8ba8e04c55560b6741b1c0d6
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-08-18 14:09:56 +0530

    ops:list-tables

---
+++ src/table-ops/list-tables.js
@@ -0,0 +1,12 @@
+const { config, DynamoDB } = require("aws-sdk");
+
+config.update({ region: "eu-west-1" });
+const dynamodb = new DynamoDB();
+
+dynamodb.listTables({}, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
